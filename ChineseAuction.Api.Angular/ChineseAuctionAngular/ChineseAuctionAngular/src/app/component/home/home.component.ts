52a494910a807eb306f00a61aa612bc14d69ea32
tk3285461 <tali3285461@gmail.com>
2026-02-15 18:25:14 +0200

    the cart and the manag user wors well !!!

--- ChineseAuction.Api.Angular/ChineseAuctionAngular/ChineseAuctionAngular/src/app/component/home/home.component.ts
+++ ChineseAuction.Api.Angular/ChineseAuctionAngular/ChineseAuctionAngular/src/app/component/home/home.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
+  links = [
+    { title: 'Manage Gifts', url: '/manager/manage-gifts', color: 'purple' },
+    { title: 'Buy / Donate', url: '/buying', color: 'green' },
+    { title: 'Raffle & Winners', url: '/manager/raffle', color: 'purple' },
+    { title: 'Reports', url: '/manager/revenue', color: 'green' }
+  ];
 }
-
-export { HomeComponent as Home };
